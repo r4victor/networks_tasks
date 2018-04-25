12e824d14a904c621da0723973121442d0c4e4af
Victor Skvortsov <vds003@gmail.com>
2018-04-25 18:10:41 +0500

    Add README, fix typo

--- tasks/dns_cache_server/cli.py
+++ tasks/dns_cache_server/cli.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-la', '--localaddress', default='127.0.0.1')
     parser.add_argument('-lp', '--localport', type=int, default=55555)
-    parser.add_argument('-ra', '--remoteadress', default='8.8.8.8')
+    parser.add_argument('-ra', '--remoteaddress', default='8.8.8.8')
     parser.add_argument('-rp', '--remoteport', type=int, default=53)
     namespace = parser.parse_args(sys.argv[1:])
     server = DNSServer(
         addr=namespace.localaddress,
         port=namespace.localport,
-        remote_addr=namespace.remoteadress,
+        remote_addr=namespace.remoteaddress,
         remote_port=namespace.remoteport)
     print('Running on {}:{}'.format(server.addr, server.port))
     server.start()
